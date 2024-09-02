22b6d0a69fd2ecabb9704fc41ea1aa7cde3a65ff
yuzhelow <yu.zhe.low@intel.com>
2024-09-02 23:20:08 +0800

    added prt

--- scripts/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "I like turtles" > malicious-file.txt
-git config --global user.name "yu.zhe.low@intel.com"
-git config --global user.name "yuzhelow"
-git add malicious-file.txt
-git commit -m "I am telling you a secret"
-git push origin HEAD:main
